a9f6ea016633f38d73665279fd025868f15182cf
Adolfo Sanchez <adolfoss87@hotmail.com>
2021-10-01 13:52:47 -0500

    fin seccion reforzamiento angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { PagesModule } from './pages/pages.module';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // AboutComponent,
+    // ContactComponent,
+    // HomeComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
